6167b88ce37ae1af8e5042abc5d882c7eb862a90
Art Mary <artmary.ir@gmail.com>
2020-03-30 23:11:42 +0430

    change precommit hook.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 export class About extends Component {
   render() {
     return (
-      <React.Fragment><h1>About pppPage</h1>
+      <React.Fragment>
+        <h1>About pppPage</h1>
         <p>lorem ipsum paragraph.</p>
       </React.Fragment>
     );
