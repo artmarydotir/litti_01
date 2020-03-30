f9a12a4de48b2292f295793c50c511a90ae1f92d
Art Mary <artmary.ir@gmail.com>
2020-03-30 23:22:38 +0430

    test.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,9 @@ export class About extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>About pppPage</h1>
+        <h1>About pppPage
+          
+        </h1>
         <p>lorem ipsum paragraph.</p>
       </React.Fragment>
     );
