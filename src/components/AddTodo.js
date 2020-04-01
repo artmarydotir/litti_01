db56829432ce35355cb37e7ed0983840dbfb638d
Art Mary <artmary.ir@gmail.com>
2020-04-01 15:41:27 +0430

    change rul.

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddTodo extends Component {
   // local-component state
   state = {
-    title: "",
+    title: '',
   };
   onChangeTitle = (e) => {
     this.setState(
@@ -16,13 +16,13 @@ class AddTodo extends Component {
   onSubmit = (e) => {
     e.preventDefault();
     // !! we ned props cuz we want add sth to parent state
-    if (this.state.title !== "") {
+    if (this.state.title !== '') {
       this.props.addTodo(this.state.title);
     } else {
-      alert("Shame on you, Empty task? Really?");
+      alert('Shame on you, Empty task? Really?');
     }
     this.setState({
-      title: "",
+      title: '',
     });
   };
   render() {
@@ -50,16 +50,16 @@ class AddTodo extends Component {
 
 // styles
 const formStyle = {
-  display: "flex",
-  padding: "10px 0",
+  display: 'flex',
+  padding: '10px 0',
 };
 const btnStt = {
-  flex: "1",
+  flex: '1',
 };
 const inpStt = {
-  border: "1px solid #c7c3c3",
-  flex: "10",
-  padding: "6px",
+  border: '1px solid #c7c3c3',
+  flex: '10',
+  padding: '6px',
 };
 AddTodo.propTypes = {
   addTodo: PropTypes.func.isRequired,
